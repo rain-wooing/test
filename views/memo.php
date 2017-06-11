573759548949c54055364eeae6ab014c92a7db6a
이다은 <rain@one-page.co.kr>
2017-06-12 01:25:28 +0900

    local commit

---
+++ views/memo.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <title>메모</title>
+    </head>
+    <body>
+        메모
+        <input type="text" name="memo_content">
+        <button type="button" class="ajax_submit_btn">등록</button>
+
+        <div class="list"></div>
+
+    </body>
+</html>
